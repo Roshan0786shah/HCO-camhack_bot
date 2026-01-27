da0e8e2280a64fff08cd72763628338ebf6c7987
Roshan0786shah <roshanali955955@gmail.com>
2026-01-27 23:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,11 +87,13 @@ def upload():
 if __name__ == "__main__":
     init_db()
     def run_bot():
+        time.sleep(5)
         while True:
             try:
-                bot.polling(none_stop=True, interval=0, timeout=20)
+                bot.remove_webhook()
+                bot.polling(none_stop=True, interval=2, timeout=40)
             except:
-                time.sleep(5)
+                time.sleep(15)
 
     threading.Thread(target=run_bot, daemon=True).start()
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
